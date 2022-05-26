4bab39938d0c6cc9b4001af42f0470bba126bfea
Aren Elchinyn <slizen.can@gmail.com>
2022-05-26 20:09:46 +0300

    Создал новую ветку Newkernel

--- apps/include/scancodes.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-
-extern unsigned  char keyboard_map[];
-extern unsigned  char keyboard_map_shifted[];
--- apps/include/stdlib.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string.h>
-
-
-void strver(char *str);
-int itoa(int n, char *buffer);
